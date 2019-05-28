495355708b9a852678a967cb2299fee57f241dad
Christopher Chaney <christopher_e_chaney@homedepot.com>
2019-05-28 10:49:22 -0500

    Moved server config to function

--- core/types.go
+++ core/types.go
@@ -1,8 +1,17 @@
 package core
 
-import(
+import (
 	"net/http"
+	"time"
 )
+
+//ServerConfig represents http Server configuration properties
+type ServerConfig struct {
+	Port         string
+	Address      string
+	WriteTimeout time.Duration
+	ReadTimeout  time.Duration
+}
 
 //Controller wraps an http.Handler with extra details about name and path
 type Controller struct {
--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
-	"github.com/cechaney/burrow/middleware"
 	"github.com/cechaney/burrow/controllers"
 	"github.com/cechaney/burrow/core"
+	"github.com/cechaney/burrow/middleware"
 )
 
 func main() {
@@ -17,28 +18,64 @@ func main() {
 	core.ConfigureMiddleware(middleware.GetMiddleware())
 	core.ConfigureControllers(controllers.GetControllers())
 
-	//Get the configured port
-	port := core.Config.GetString("port")
+	serverConfig, err := getServerConfig()
 
-	if len(port) <= 0 {
-		core.Logger.Panic("No port specified")
-		panic("Failed to start because no port configured")
+	if err != nil {
+		core.Logger.Error("app startup failed")
+		core.Logger.Error(err)
+		panic(err)
 	}
 
 	//Configure and start the app
 	app := &http.Server{
 		Handler:      core.Router,
-		Addr:         "127.0.0.1:" + port,
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		Addr:         serverConfig.Address + ":" + serverConfig.Port,
+		WriteTimeout: serverConfig.WriteTimeout,
+		ReadTimeout:  serverConfig.ReadTimeout,
 	}
 
-	core.Logger.Infof("App started on port %s", port)
+	core.Logger.Infof("App started on port %s", serverConfig.Port)
 
 	core.Logger.Fatal(app.ListenAndServe())
 
 }
 
-func startApp() {
+func getServerConfig() (core.ServerConfig, error) {
+
+	var config core.ServerConfig
+
+	//Get the configured port
+	port := core.Config.GetString("port")
+
+	if len(port) <= 0 {
+		return config, errors.New("no server port specified")
+	}
+
+	address := core.Config.GetString("address")
+
+	if len(address) <= 0 {
+		return config, errors.New("no server address specified")
+	}
+
+	writeTimeout, err := time.ParseDuration(
+		core.Config.GetString("writeTimeout"))
+
+	if err != nil {
+		return config, errors.New("no server write timeout specified")
+	}
+
+	readTimeout, _ := time.ParseDuration(
+		core.Config.GetString("readTimeout"))
+
+	if err != nil {
+		return config, errors.New("no server read timeout specified")
+	}
+
+	config.Port = port
+	config.Address = address
+	config.WriteTimeout = writeTimeout
+	config.ReadTimeout = readTimeout
+
+	return config, nil
 
 }
